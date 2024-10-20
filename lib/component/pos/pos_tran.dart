84afeba9415de1d503bc7f5663c4610d754542d0
irawanmurjayanto <162110432+irawanmurjayanto@users.noreply.github.com>
2024-10-20 20:32:23 +0700

    oct24

--- lib/component/pos/pos_tran.dart
+++ lib/component/pos/pos_tran.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'dart:io';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+
+import 'package:flutter_pos_new/component/warning.dart';
 
 class Pos_Tran extends StatefulWidget {
   const Pos_Tran({super.key});
@@ -8,8 +15,54 @@ class Pos_Tran extends StatefulWidget {
 }
 
 class _Pos_TranState extends State<Pos_Tran> {
-  final NoRef = DateTime.now();
   final _Text_Cust = TextEditingController();
+  getCustomer() async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Customer Name'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _Text_Cust,
+                decoration: InputDecoration(
+                    hintText: 'Customer Name Search',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: BorderSide(style: BorderStyle.solid))),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  final _barcodecode = TextEditingController();
+  int? _jumlah;
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          "#ff0000", "Cancel", true, ScanMode.BARCODE);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _barcodecode.text = barcodeScanRes;
+      _jumlah = 1;
+    });
+  }
+
+  final NoRef = DateTime.now();
+
   String? no_pos;
 
   getNoref() async {
@@ -38,50 +91,66 @@ class _Pos_TranState extends State<Pos_Tran> {
       ),
       appBar: AppBar(
         title: const Text(
-          'Pos Trnasaction',
+          'Pos Transaction',
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+              onPressed: () {
+                scanBarcodeNormal();
+              },
+              icon: Icon(Icons.barcode_reader)),
+          IconButton(
+              onPressed: () {
+                getCustomer();
+              },
+              icon: Icon(Icons.barcode_reader))
+        ],
       ),
-      body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: double.infinity,
-          decoration: BoxDecoration(
-              color: Colors.blue, border: Border.all(style: BorderStyle.solid)),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-                child: WCust_Name(),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(
-                no_pos!,
-                style: const TextStyle(color: Colors.white),
-              ),
-              Header(),
-              const SizedBox(height: 5,),
-              WCalc()
-            ],
-          )),
+      body: SingleChildScrollView(
+        child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.blue,
+                border: Border.all(style: BorderStyle.solid)),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 55,
+                  child: WCust_Name(),
+                ),
+                
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  no_pos!,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                Header(),
+                const SizedBox(
+                  height: 5,
+                ),
+                WCalc()
+              ],
+            )),
+      ),
     );
   }
 
-   Widget WCalc() {
+  Widget WCalc() {
     return Container(
-      margin: const EdgeInsets.all(5),
-      height: MediaQuery.of(context).size.height/1.8,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border.all(style: BorderStyle.none),
-        color: Colors.white
-
-      ),
-      child: const Center(child: Text('Text'),)
-    );
+        margin: const EdgeInsets.all(5),
+        height: MediaQuery.of(context).size.height / 1.8,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            border: Border.all(style: BorderStyle.none), color: Colors.white),
+        child: const Center(
+          child: Text('Text'),
+        ));
   }
 
   Widget Header() {
@@ -128,6 +197,7 @@ class _Pos_TranState extends State<Pos_Tran> {
             width: 5,
           ),
           Expanded(
+              child: GestureDetector(
             child: Container(
               padding: const EdgeInsets.all(5),
               decoration: const BoxDecoration(color: Colors.black),
@@ -135,13 +205,14 @@ class _Pos_TranState extends State<Pos_Tran> {
                   style: TextStyle(color: Colors.white),
                   textAlign: TextAlign.center),
             ),
-          ),
+            onTap: () {
+              getCustomer();
+            },
+          )),
         ],
       ),
     );
   }
-
- 
 
   Widget WCust_Name() {
     return Container(
@@ -149,25 +220,32 @@ class _Pos_TranState extends State<Pos_Tran> {
         //   color: Colors.white
         // ),
         padding: const EdgeInsets.all(7),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: TextFormField(
-                controller: _Text_Cust,
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    labelStyle: const TextStyle(color: Colors.black, fontSize: 12),
-                    labelText: 'Customer Name',
-                    hintText: 'Customer Name',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: const BorderSide(style: BorderStyle.solid))),
+       
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: TextField(
+                  
+                  controller: _Text_Cust,
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(onPressed: () {
+                      getCustomer();
+                    }, icon: Icon(Icons.card_giftcard)),
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintStyle: TextStyle(fontSize: 12,fontWeight: FontWeight.bold), 
+                      hintText: 'Customer Name',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                          borderSide:
+                              const BorderSide(style: BorderStyle.solid))),
+                ),
               ),
-            ),
-          ],
-        ));
+            ],
+          )
+          );
+         
   }
 }
